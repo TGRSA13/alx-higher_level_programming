1ff8952bd48831c4e1164a367138c6d8e9b3315f
TGRSA13 <tgmchunu@gmail.com>
2023-11-30 09:45:58 -0800

    new updates

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
-a = 98
-"""Simple variable
-"""
 
-print("The value of a is:", a)
+import importlib
+
+def main():
+    # Importing the variable 'a' from variable_load_5.py
+    module_name = 'variable_load_5'
+    variable_name = 'a'
+
+    try:
+        module = importlib.import_module(module_name)
+        a = getattr(module, variable_name)
+
+        print("The value of 'a' is:", a)
+    except (ModuleNotFoundError, AttributeError):
+        print(f"Error: Unable to import variable '{variable_name}' from '{module_name}.py'.")
+
+if __name__ == "__main__":
+    main()
