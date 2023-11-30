4356e319cbb824ee11376fb22cd130fc509a4599
TGRSA13 <tgmchunu@gmail.com>
2023-11-30 09:34:24 -0800

    file updated

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def add(a, b):
     """My addition function
     Args:
@@ -7,8 +8,7 @@ def add(a, b):
     Returns:
         The return value. a + b
     """
-    return (a + b)
-
+    return a + b
 
 def sub(a, b):
     """My subtraction function
@@ -18,8 +18,7 @@ def sub(a, b):
     Returns:
         The return value. a - b
     """
-    return (a - b)
-
+    return a - b
 
 def mul(a, b):
     """My multiplication function
@@ -29,15 +28,20 @@ def mul(a, b):
     Returns:
         The return value. a * b
     """
-    return (a * b)
-
+    return a * b
 
 def div(a, b):
     """My division function
     Args:
-        a: first integer
-        b: second integer
+        a: numerator
+        b: denominator
     Returns:
         The return value. a / b
     """
-    return int(a / b)
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    return a / b
+
+if __name__ == "__main__":
+    # You can test your functions here if needed
+    pass
