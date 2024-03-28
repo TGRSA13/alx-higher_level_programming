03b6e037ffbc5ca8a0939dafb7cc7c853ad2cc2d
TGRSA13 <tgmchunu@gmail.com>
2024-03-28 10:41:09 -0700

    0x11 Python network 1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status."""
+import urllib.request
+
+
+if __name__ == "__main__":
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        data = response.read()
+        print("Data response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode("utf-8")))
